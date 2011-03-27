25947236fe2b7919330fde3aeabfd1ed6e39ae9f
Kevin Glynn <kevin.glynn@twigletsoftware.com>
2011-03-27 14:44:47 +0200

    Make trial version from master branch

--- CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/Templates.cs
+++ CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/Templates.cs
@@ -19,6 +19,8 @@ itsmine(now, includeDate) ::= <<
 //
 // This file was translated from C# to Java by CS2J (http://www.cs2j.com).
 //
+// This code is to be used for evaluation of the CS2J tool ONLY.
+//
 // For more information about CS2J please contact info@twigletsoftware.com
 <if(includeDate)>
 //
