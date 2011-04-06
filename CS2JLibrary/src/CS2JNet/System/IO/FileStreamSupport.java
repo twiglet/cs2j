1bb7b324422cd208261ab7b7bda7d657ffd6caba
Kevin Glynn <kevin.glynn@twigletsoftware.com>
2011-04-06 16:30:12 +0200

    Support FileStreams

--- CS2JLibrary/src/CS2JNet/System/IO/FileStreamSupport.java
+++ CS2JLibrary/src/CS2JNet/System/IO/FileStreamSupport.java
@@ -21,8 +21,76 @@
 
 package CS2JNet.System.IO;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
+import CS2JNet.JavaSupport.CS2JRunTimeException;
+
+/**
+ * 
+ * @author kevin.glynn@twiglet.com
+ *
+ */
 public class FileStreamSupport  {
 
 
+	private FileInputStream inputStream = null;
+	private FileOutputStream outputStream = null;
+	
+	/***
+	 * Initializes a new instance of the FileStreamSupport class with the specified path, 
+	 * creation mode, and read/write permission
+	 * 
+	 * This class mimics System.IO.FileStream
+	 * 
+	 * If mode is read only then set readStream, if write only then set writeStream, 
+	 * if readWrite throw an exception :(
+	 * @param path
+	 * @param mode
+	 * @param access
+	 * @throws FileNotFoundException 
+	 * @throws CS2JRunTimeException 
+	 */
+	public FileStreamSupport(String path, FileMode mode, FileAccess access) throws FileNotFoundException, CS2JRunTimeException {
+		switch (access) {
+		case Read:
+			setInputStream(new FileInputStream(path));
+			break;
+		case Write:
+			setOutputStream(new FileOutputStream(path, mode == FileMode.Append));
+			break;
+		case ReadWrite:
+		default:
+			throw new CS2JRunTimeException("CS2J: Read / Write FileStreams are not yet supported");
+		}
+	}
+
+	/**
+	 * @param inputStream the inputStream to set
+	 */
+	public void setInputStream(FileInputStream inputStream) {
+		this.inputStream = inputStream;
+	}
+
+	/**
+	 * @return the inputStream
+	 */
+	public FileInputStream getInputStream() {
+		return inputStream;
+	}
+
+	/**
+	 * @param outputStream the outputStream to set
+	 */
+	public void setOutputStream(FileOutputStream outputStream) {
+		this.outputStream = outputStream;
+	}
+
+	/**
+	 * @return the outputStream
+	 */
+	public FileOutputStream getOutputStream() {
+		return outputStream;
+	}
 }
