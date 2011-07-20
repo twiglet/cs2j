fc56b9f5c305589d283b89f9550a884d8d623baa
Kevin Glynn <kevin.glynn@twigletsoftware.com>
2011-07-20 15:33:14 +0200

    make trial from release

--- CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/CS2JMain.cs
+++ CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/CS2JMain.cs
@@ -32,7 +32,7 @@ namespace Twiglet.CS2J.Translator
 {
     class CS2J
     {
-       private const string VERSION = "2011.2.3";
+       private const string VERSION = "2011.2.3trial";
        private static DirectoryHT<TypeRepTemplate> AppEnv { get; set; }
        private static CS2JSettings cfg = new CS2JSettings();
        private static StringTemplateGroup templates = null;
@@ -358,7 +358,7 @@ namespace Twiglet.CS2J.Translator
 			// Suck in translation file
             Stream txStream = new FileStream(fullName, FileMode.Open, FileAccess.Read);
 
-             if (numLines < numLines - 1)
+             if (numLines < numLines + 1)
              {
                 // TRIAL ONLY
                 // Create a new XML document.
@@ -421,7 +421,7 @@ namespace Twiglet.CS2J.Translator
         }
 		
        private static string limit(string inp) {
-          if (numLines > numLines - 1)
+          if (numLines > numLines + 1)
              return inp;
           // TRIAL ONLY
           String[] lines = inp.Split(newLines, numLines+1, StringSplitOptions.None);
