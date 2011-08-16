9df8b1e620b1e4f8a97206ebe470fad2ee0a1c41
Kevin Glynn <kevin.glynn@twigletsoftware.com>
2011-08-16 15:05:04 +0200

    Make Devel version

--- CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/CS2JMain.cs
+++ CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/CS2JMain.cs
@@ -32,7 +32,7 @@ namespace Twiglet.CS2J.Translator
 {
     class CS2J
     {
-       private const string VERSION = "2011.2.3";
+       private const string VERSION = "2011.2.0.Devel";
        private static DirectoryHT<TypeRepTemplate> AppEnv { get; set; }
        private static CS2JSettings cfg = new CS2JSettings();
        private static StringTemplateGroup templates = null;
