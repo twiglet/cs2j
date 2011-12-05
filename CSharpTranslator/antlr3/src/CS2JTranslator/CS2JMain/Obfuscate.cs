1518806ac95ab042d5b092303f2cb9f46de03041
Kevin Glynn <kevin.glynn@twigletsoftware.com>
2011-12-05 13:44:02 +0100

    Don't obfuscate TypeRep Templates

--- CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/Obfuscate.cs
+++ CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/Obfuscate.cs
@@ -3,3 +3,5 @@ using System.Reflection;
 // Control Eazfuscator.NET
 
 [assembly: Obfuscation(Feature = "code control flow obfuscation", Exclude = false)]
+[assembly: Obfuscation(Feature = "Apply to type Twiglet.CS2J.Translator.TypeRep.*: all", Exclude = true, ApplyToMembers = true)]
+
