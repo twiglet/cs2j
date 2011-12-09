e1074880785bf9da2a6f44179e06b2e57e690018
Kevin Glynn <kevin.glynn@twigletsoftware.com>
2011-12-09 13:50:16 +0100

    fix obfuscation directive

--- CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/Obfuscate.cs
+++ CSharpTranslator/antlr3/src/CS2JTranslator/CS2JMain/Obfuscate.cs
@@ -4,6 +4,6 @@ using System.Reflection;
 
 [assembly: Obfuscation(Feature = "code control flow obfuscation", Exclude = false)]
 [assembly: Obfuscation(Feature = "Apply to type Twiglet.CS2J.Translator.TypeRep.*: all", Exclude = true, ApplyToMembers = true)]
-[assembly: Obfuscation(Feature = "Apply to type Twiglet.CS2J.Translator.Transform.JavaMaker", Exclude = true, ApplyToMembers = true)]
-[assembly: Obfuscation(Feature = "Apply to type AntlrCSharp.csParser", Exclude = true, ApplyToMembers = true)]
+[assembly: Obfuscation(Feature = "Apply to type Twiglet.CS2J.Translator.Transform.JavaMaker: all", Exclude = true, ApplyToMembers = true)]
+[assembly: Obfuscation(Feature = "Apply to type AntlrCSharp.csParser: all", Exclude = true, ApplyToMembers = true)]
 
